dbaf42f5a5e746a047d443427cc8f9079b0f5034
Danial14 <daniyalghani5@gmail.com>
2026-01-27 03:44:56 -0800

    working on store

---
+++ lib/common/widgets/tabbar/custom_tabbar.dart
@@ -0,0 +1,31 @@
+import 'package:apna_store/utils/constants/colors.dart';
+import 'package:apna_store/utils/devices/device_utility.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/helpers/helper_functions.dart';
+
+class CustomTabbar extends StatelessWidget implements PreferredSizeWidget{
+  const CustomTabbar({super.key,
+  required this.tabs
+  });
+  final List<Widget> tabs;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = AppHelperFunctions.isDarkMode(context);
+    return Material(
+      color: isDark ? AppColors.light : AppColors.grey,
+      child: TabBar(
+        tabs: tabs,
+        isScrollable: true,
+        indicatorColor: AppColors.primary,
+        labelColor: isDark ? AppColors.grey : AppColors.dark,
+        unselectedLabelColor: AppColors.darkGrey,
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(DeviceUtils.getAppbarHeight());
+}
--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,5 +1,6 @@
 import 'package:apna_store/common/widgets/appbar/customappbar.dart';
 import 'package:apna_store/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:apna_store/common/widgets/layout/grid_layout.dart';
 import 'package:apna_store/common/widgets/products/product_cart_widget/custom_cart.dart';
 import 'package:apna_store/common/widgets/section_heading/section_heading.dart';
 import 'package:apna_store/utils/constants/image_strings.dart';
@@ -9,6 +10,7 @@ import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 import '../../../../common/widgets/images/circular_image.dart';
 import '../../../../common/widgets/search_container/search_container.dart';
+import '../../../../common/widgets/tabbar/custom_tabbar.dart';
 import '../../../../common/widgets/texts/brand_title_text.dart';
 import '../../../../utils/constants/colors.dart';
 import '../../../../utils/constants/enums.dart';
@@ -20,78 +22,104 @@ class Store extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = AppHelperFunctions.isDarkMode(context);
-    return Scaffold(
-      appBar: CustomAppbar(
-        title: Text("Store",
-        style: Theme.of(context).textTheme.headlineMedium,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: CustomAppbar(
+          title: Text("Store",
+          style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: [
+            CustomCartWidget(onPressed: (){}, iconColor: AppColors.dark)
+          ]
         ),
-        actions: [
-          CustomCartWidget(onPressed: (){}, iconColor: AppColors.dark)
-        ]
-      ),
-      body: NestedScrollView(headerSliverBuilder: (ctx, isInnerScrollable){
-        return [
-          SliverAppBar(
-            pinned: true,
-            floating: true,
-            backgroundColor: isDark ? AppColors.dark : AppColors.light,
-            expandedHeight: 440,
-            automaticallyImplyLeading: false,
-            flexibleSpace: Padding(padding: EdgeInsets.all(Sizes.defaultSpace),
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                // Search bar
-                SizedBox(height: Sizes.spaceBetweenItems,),
-                SearchContainer(
-                  text: "Search",
-                  showBorder: true,
-                  icon: Iconsax.search_normal,
-                  showBackground: false,
-                  padding: EdgeInsets.zero,
-                ),
-                SizedBox(height: Sizes.spaceBetweenSections,),
-                // Feature brands
-                HeadingSection(text: "Feature Brands",
-                showActionButton: true,
-                  onPressed: (){
-
-                  },
-                ),
-                SizedBox(height: Sizes.spaceBetweenItems / 1.5),
-                CircularShape(
-                  padding: const EdgeInsets.all(Sizes.sm),
-                  backgroundColor: Colors.transparent,
-                  child: Row(
-                    children: [
-                      CircularImage(isDark: isDark,
-                      image: ImageStrings.productImageOne,
-                        overlayColor: isDark ? AppColors.light : AppColors.dark,
-                        backgroundColor: Colors.transparent,
-                      ),
-                      const SizedBox(width: Sizes.spaceBetweenItems / 2),
-                      // Text
-                      Column(
-                        children: [
-                          const BrandTitleText(title: "Nike",
-                          brandTextSize: TextSizes.large,
-                          ),
-                          Text(
-                            "256 products",
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context).textTheme.labelMedium
-                          )
-                        ],
-                      )
-                    ],
+        body: NestedScrollView(headerSliverBuilder: (ctx, isInnerScrollable){
+          return [
+            SliverAppBar(
+              pinned: true,
+              floating: true,
+              backgroundColor: isDark ? AppColors.dark : AppColors.light,
+              expandedHeight: 440,
+              automaticallyImplyLeading: false,
+              flexibleSpace: Padding(padding: EdgeInsets.all(Sizes.defaultSpace),
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  // Search bar
+                  SizedBox(height: Sizes.spaceBetweenItems,),
+                  SearchContainer(
+                    text: "Search",
+                    showBorder: true,
+                    icon: Iconsax.search_normal,
+                    showBackground: false,
+                    padding: EdgeInsets.zero,
                   ),
-                )
-              ]
-            ),
-            ),
-          )
-        ];
-      }, body: Container()),
+                  SizedBox(height: Sizes.spaceBetweenSections,),
+                  // Feature brands
+                  HeadingSection(text: "Feature Brands",
+                  showActionButton: true,
+                    onPressed: (){
+
+                    },
+                  ),
+                  SizedBox(height: Sizes.spaceBetweenItems / 1.5),
+                  CustomGrid(itemCount: 4, itemBuilder: (ctx, index){
+                    return GestureDetector(
+                      onTap: (){
+
+                      },
+                      child: CircularShape(
+                        padding: const EdgeInsets.all(Sizes.sm),
+                        backgroundColor: Colors.transparent,
+                        child: Row(
+                          children: [
+                            Flexible(
+                              child: CircularImage(isDark: isDark,
+                                image: ImageStrings.productImageOne,
+                                overlayColor: isDark ? AppColors.light : AppColors.dark,
+                                backgroundColor: Colors.transparent,
+                              ),
+                            ),
+                            const SizedBox(width: Sizes.spaceBetweenItems / 2),
+                            // Text
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const BrandTitleText(title: "Nike",
+                                    brandTextSize: TextSizes.large,
+                                  ),
+                                  Text(
+                                      "256 products",
+                                      overflow: TextOverflow.ellipsis,
+                                      style: Theme.of(context).textTheme.labelMedium
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  maxExtent: 80,
+                  ),
+
+                ]
+              ),
+              ),
+              bottom: CustomTabbar(tabs: [
+                Tab(text: "Sports",),
+                Tab(text: "Furniture",),
+                Tab(text: "Electronics",),
+                Tab(text: "Cosmetics",),
+                Tab(text: "Clothes",)
+              ],),
+            )
+          ];
+        }, body: Container()),
+      ),
     );
   }
 }
